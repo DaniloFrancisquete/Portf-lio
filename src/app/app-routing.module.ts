a0a9072994a04b048b666004d36e11dd08eff65e
danilofrancisquete <danilofrancisquete@hotmail.com>
2024-08-30 15:23:32 -0300

    courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '',pathMatch:'full',redirectTo:'dashboard'},
   { path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) },
   { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'courses', loadChildren: () => import('./pages/courses/courses.module').then(m => m.CoursesModule) },
